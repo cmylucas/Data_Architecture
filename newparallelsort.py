f4ab47d3085ce2416d75ee17fd43c253d10fcc7b
cmylucas <155979046+cmylucas@users.noreply.github.com>
2024-03-28 10:13:56 -0400

    Update newparallelsort.py

--- newparallelsort.py
+++ newparallelsort.py
@@ -8,7 +8,7 @@ import shutil
 file_path = "/usb/data1.set"
 N = int(sys.argv[1]) # Total amount of numbers to sort
 n = int(sys.argv[2]) # Number of divisions
-nodes = ["192.168.10.1", "192.168.10.10", "192.168.10.20", "192.168.10.30", "192.168.10.40"]
+nodes = ["192.168.10.10", "192.168.10.20", "192.168.10.30", "192.168.10.40"]
 
 def bubble(path):
     with open(path, 'r') as file:
