ccffa25a5baa585dc773f0215103bb0829342a58
cmylucas <155979046+cmylucas@users.noreply.github.com>
2024-04-16 10:27:25 -0400

    Update testread.py

--- testread.py
+++ testread.py
@@ -3,21 +3,35 @@ import sys
 import dispy
 import socket
 import math
+import shutil
+import os
+from tqdm import tqdm
 
 file_path = "/usb/data1.set"
 N = int(sys.argv[1]) # Total amount of numbers to sort
+chunk_size = int(sys.argv[2])
+n = int(N/chunk_size)
 nodes = ["192.168.10.1", "192.168.10.10", "192.168.10.20", "192.168.10.30", "192.168.10.40"]
 
 def main():
     print(file_path)
-    numbers = []
+    total_numbers = 0
     start_time = time.time()
-    with open(file_path, 'r') as file:
-        for x in range(N):
-            num = int(file.readline())
-            numbers.append(num)
-    print(f"Reading took {(time.time() - start_time) / 60} minutes")
-    print(numbers)
+    with open(file_path, 'r') as input:
+        for index in range(n):
+            open(f"/mnt/shared/sorting/segment{index}.txt", 'w').close()
+            with open(f"/mnt/shared/sorting/segment{index}.txt", 'a') as file:
+                for x in range(math.ceil(N/n)):
+                    if total_numbers == N:
+                        break
+                    num = int(input.readline())
+                    file.write(f"{num}\n")
+                    total_numbers += 1
+                file.close()
+            index += 1
+                
+    reading_time = (time.time() - start_time) / 60
+    print(f"Reading took {reading_time} minutes")
 
 
 if __name__ == "__main__":
