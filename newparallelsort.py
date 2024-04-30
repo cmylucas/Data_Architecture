113645bcc20305a51046e6cb5e3aef9e088b2687
cmylucas <155979046+cmylucas@users.noreply.github.com>
2024-04-30 10:45:23 -0400

    Update newparallelsort.py

--- newparallelsort.py
+++ newparallelsort.py
@@ -10,70 +10,79 @@ from tqdm import tqdm
 file_path = "/usb/data1.set"
 N = int(sys.argv[1]) # Total amount of numbers to sort
 chunk_size = int(sys.argv[2])
-n = N/chunk_size
+n = math.ceil(N/chunk_size) # number of chunks
 # n = int(sys.argv[2]) # Number of divisions
 nodes = ["192.168.10.10", "192.168.10.20", "192.168.10.30", "192.168.10.40"]
+file_size = os.path.getsize(file_path)
 
+# checks if end of file
 def checkRead(num):
     if num != '':
         num = int(num)
     return num
 
-def bubble(path):
-    arr =[]
-    with open(path, 'r') as file:
-        for line in file:
-            number = line.strip()
-            arr.append(int(number))
-    # with open(path, 'r') as file:
-    #     num = checkRead2(file.readline())
-    #     arr.append(num)
-    #     while num:
-    #         num = checkRead2(file.readline())
-    #         arr.append(num)
-    length = len(arr)
-    hostname = socket.gethostname()
-    for i in range(length - 1):
-        swapped = False
-        for j in range(0, length - i - 1):
-            if arr[j] > arr[j + 1]:
-                swapped = True
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
-        if not swapped:
-            with open(path, 'w') as file:
-                for num in arr:
-                    file.write(f"{num}\n")
-            return (path, hostname)
+# takes in chunk number, reads the chunk and bubble sorts
+def chunk_and_bubble(chunk_num):
+    def get_chunk(chunk_number):
+        start_pos = chunk_size * chunk_number
+        end_pos = chunk_size * (chunk_number + 1)
+        with open(file_path, 'r') as f:
+            if chunk_number != 0:
+                f.seek(start_pos)
+                byte = f.read(1)
+                while not byte == "\n":
+                    start_pos += 1
+                    f.seek(start_pos)
+                    byte = f.read(1)
+                start_pos += 1
+    
+            if end_pos >= file_size:
+                end_pos = file_size
+            else:
+                f.seek(end_pos)
+                byte = f.read(1)
+                while not byte == "\n":
+                    end_pos += 1
+                    f.seek(end_pos)
+                    byte = f.read(1)
+                end_pos += 1
+    
+            f.seek(start_pos)
+            arr = []
+            while f.tell() <= end_pos:
+                arr.append(int(f.readline()))
+            return arr
+    def bubble(arr, path):
+        length = len(arr)
+        hostname = socket.gethostname()
+        for i in range(length - 1):
+            swapped = False
+            for j in range(0, length - i - 1):
+                if arr[j] > arr[j + 1]:
+                    swapped = True
+                    arr[j], arr[j + 1] = arr[j + 1], arr[j]
+            if not swapped:
+                with open(path, 'w') as file:
+                    for num in arr:
+                        file.write(f"{num}\n")
+                return (path, hostname)
+    
+        with open(path, 'w') as file:
+            for num in arr:
+                file.write(f"{num}\n")
+        return (path, hostname)
 
-    with open(path, 'w') as file:
-        for num in arr:
-            file.write(f"{num}\n")
-    return (path, hostname)
+    return bubble(arr = get_chunk(chunk_num), path = f"/mnt/shared/sorting/segment{chunk_num}.txt")
 
 def main():
-    cluster = dispy.JobCluster(bubble, nodes = nodes, host = "192.168.10.1")
-
     print(file_path)
     total_numbers = 0
     jobs = []
     start_time = time.time()
-    with open(file_path, 'r') as input:
-        for index in range(n):
-            open(f"/mnt/shared/sorting/segment{index}.txt", 'w').close()
-            with open(f"/mnt/shared/sorting/segment{index}.txt", 'a') as file:
-                for x in range(math.ceil(N/n)):
-                    if total_numbers == N:
-                        break
-                    num = int(input.readline())
-                    file.write(f"{num}\n")
-                    total_numbers += 1
-                file.close()
-            job = cluster.submit(f"/mnt/shared/sorting/segment{index}.txt")
-            jobs.append(job)
-            index += 1
-                
-    reading_time = (time.time() - start_time) / 60
-    print(f"Reading took {reading_time} minutes")
+    cluster = dispy.JobCluster(chunk_and_bubble, nodes = nodes, host = "192.168.10.1")
+    for chunk_num in range(n):
+        job = cluster.submit(chunk_num)
+        jobs.append(job)
 
     stored_path = "/mnt/shared/sorting/final.txt"
     temp_path = "/mnt/shared/sorting/temp.txt"
